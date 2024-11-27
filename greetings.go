91845b7da6382acf554afeff8b0b9d70ed0da689
tea06194 <tea06194@gmail.com>
2024-11-27 19:17:16 +0300

    multiple peoples v0.0.6

--- greetings.go
+++ greetings.go
@@ -6,7 +6,6 @@ import (
 	"math/rand"
 )
 
-// H - h test
 func Hello(name string) (string, error) {
 	if name == "" {
 		return "", errors.New("empty name")
@@ -15,6 +14,21 @@ func Hello(name string) (string, error) {
 	message := fmt.Sprintf(randomFormat(), name)
 	
 	return message, nil
+}
+
+func Hellos(names []string) (map[string]string, error) {
+	messages := make(map[string]string)
+
+	for _, name := range names {
+		message, err := Hello(name)
+
+		if err != nil {
+			return nil, err
+		}
+
+		messages[name] = message
+	}
+	return messages, nil
 }
 
 func randomFormat() string {
